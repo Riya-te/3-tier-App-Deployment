8f32ed92535153500b7f8fc07559f25bd6fa6cec
Riya <riyakumari1011.2006@gmail.com>
2026-03-29 22:34:15 +0530

    3-tier done

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'my3tierdb.c3wick2u0aq4.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'root123456',
+    DB_DATABASE : 'webappdb'
+});
